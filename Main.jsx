e1671fd1d122a664d91c32a99d8685f0ea9cbdd5
Aniket1703 <you@example.com>
2025-06-22 15:17:51 +0530

    Auto-push: New - Main.jsx

---
+++ Main.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import toggleDrawer from ''
+
+const Main = () => {
+const [openDrawer,setOpenDrawer]=useState(true);
+
+const toggleDrawer = () =>{
+  setOpenDrawer(prevState => !prevState)
+};
+
+
+
+  return (
+    <div>
+        <Header toggleDrawer={toggleDrawer}/>
+
+        <Sidebar openDrawer={openDrawer}/>
+        <div>Display Mails</div>
+    </div>
+  )
+}
+
+export default Main
